119026d2dad7dbc157bde15a8c3b6d6944b26cbb
JuDonNou@gmail.com <JuDonNou@gmail.com@e051e0af-a3b7-5e5d-cbc4-935b9abf3bb2>
2012-11-09 08:19:08 +0000

    Freitag

--- trunk/hm/src/edu/hm/jurkowsk/excercise_4/Fakultaet_ID.java
+++ trunk/hm/src/edu/hm/jurkowsk/excercise_4/Fakultaet_ID.java
@@ -1,17 +1,13 @@
-package edu.hm.jurkowsk.excercise_4;
-
-import edu.hm.jurkowsk.excercise_2.In;
-
 /**
  * @author Benjamin Jurkowski (jurkowsk@hm.edu)
  * @date 07.11.2012
  * 
- * Faktultäten:
+ * FaktultÃ¤ten:
  * 01 Architektur
  * 02 Bauingenieurwesen
  * 03 Maschinenbau, Fahrzeugtechnik, Flugzeugtechnik
  * 04 Elektrotechnik und Informationstechnik
- * 05 Versorgungs- und Gebäudetechnik, Verfahrenstechnik Papier und Verpackung, Druck- und Medientechnik
+ * 05 Versorgungs- und GebÃ¤udetechnik, Verfahrenstechnik Papier und Verpackung, Druck- und Medientechnik
  * 06 Feinwerk- und Mikrotechnik, Physikalische Technik
  * 07 Informatik und Mathematik
  * 08 Geoinformation
@@ -19,62 +15,49 @@ import edu.hm.jurkowsk.excercise_2.In;
  * 10 Betriebswirtschaft
  * 11 Angewandte Sozialwissenschaften
  * 12 Design
- * 13 Studium Generale und Interdisziplinäre Studien
+ * 13 Studium Generale und InterdisziplinÃ¤re Studien
  * 14 Tourismus
  * 
  */
 public class Fakultaet_ID {
 	public static void main(String[] args) {
-		int fid = In.readInt();
-		
-		switch ( fid )	{
-			case 0:
-				break;
-			case 1:
-				System.out.println("Architektur");
-				break;
-			case 2:
-				System.out.println("Bauingenieurwesen");
-				break;
-			case 3:
-				System.out.println("Maschinenbau, Fahrzeugtechnik, Flugzeugtechnik");
-				break;
-			case 4:
-				System.out.println("Elektrotechnik und Informationstechnik");
-				break;
-			case 5:
-				System.out.println("Versorgungs- und Gebäudetechnik, Verfahrenstechnik Papier und Verpackung, Druck- und Medientechnik");
-				break;
-			case 6:
-				System.out.println("Feinwerk- und Mikrotechnik, Physikalische Technik");
-				break;
-			case 7:
-				System.out.println("Informatik und Mathematik");
-				break;
-			case 8:
-				System.out.println("Geoinformation");
-				break;
-			case 9:
-				System.out.println("Wirtschaftsingenieurwesen");
-				break;
-			case 10:
-				System.out.println("Betriebswirtschaft");
-				break;
-			case 11:
-				System.out.println("Angewandte Sozialwissenschaften");
-				break;
-			case 12:
-				System.out.println("Design");
-				break;
-			case 13:
-				System.out.println("Studium Generale und Interdisziplinäre Studien");
-				break;
-			case 14:
-				System.out.println("Tourismus");
-				break;
-			default:
-				System.out.println("Ungültige ID.");
-				
-		}
+		int fid;
+		do{
+			System.out.println("Bitte geben Sie die ID der FakultÃ¤t ein:");
+			fid = In.readInt();
+			switch ( fid )	{
+				case 0: 
+					System.out.println("Programm wird terminiert");
+					break;
+				case 1:
+				case 2:
+				case 3:
+				case 4:
+				case 5:
+				case 6:
+				case 8:
+					System.out.println("FaktultÃ¤t " + fid + " - Bereich: Technik");
+					break;
+				case 7:
+				case 9:
+				case 10:
+				case 14:
+					System.out.println("FaktultÃ¤t " + fid + " - Bereich: Wirtschaft");
+					break;
+				case 11:
+					System.out.println("FaktultÃ¤t " + fid + " - Bereich: Soziales");
+					break;
+				case 12:
+					System.out.println("FaktultÃ¤t " + fid + " - Bereich: Design");
+					break;
+				case 13:
+					System.out.println("FaktultÃ¤t " + fid + " - Bereich: Generale");
+					break;
+				default:
+					System.out.println(fid + " ist eine ungÃ¼ltige ID.");
+					break;
+					
+			}
+		}while(fid>0);
 	}
 }
